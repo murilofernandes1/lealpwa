ab239947caa7487483d025c0dd213270254c7821
Murilo <152417228+murilofernandes1@users.noreply.github.com>
2025-10-26 22:44:20 -0300

    fixed API.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "https://backend-1yxu.onrender.com",
-=======
-  baseURL: "https://backend-1yxu.onrender.com/",
->>>>>>> 76c942132ec804cfaf590c906d3768b1586551b7
-});
+  baseURL: "https://backend-1yxu.onrender.com"})
+
 
 export default api;
